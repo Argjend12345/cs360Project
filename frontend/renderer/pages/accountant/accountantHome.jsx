0ef423ad6b1b289a8a5f34d076060ab7fb9a5452
Alexander Luna <alexanderfluna@outlook.com>
2023-12-04 16:57:11 -0500

    Modified the pages

--- frontend/renderer/pages/accountant/accountantHome.jsx
+++ frontend/renderer/pages/accountant/accountantHome.jsx
@@ -1,20 +1,50 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import Styles1 from '../../stylesheets/buttonComponent.module.scss';
+import Router  from 'next/router';
 
 function accountantHome() {
+
+  const logOut = async () => {
+    try {
+        Router.push('/loginPage');    
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <React.Fragment>
       <Head>
         <title>Accountant View</title>
       </Head>
 
+      <div style={{textAlign: "right"}}>
+        <button onClick={logOut} className={`${Styles1.button}`}>
+          Log out
+        </button>
+        </div>
+
       <div style={{textAlign: "center"}}>
         <img style={{padding:"0px 0px 0px 0px", height: "200px", width:"350px"}}src="/images/logo.png"/>
       </div>
 
       <div style={{textAlign: "center"}}>
-        <h1>This is the ACCOUNTANT Page!</h1>
+
+        <h1>Hello Accountant!</h1>
+        <button className={`${Styles1.button}`} style={{padding: "20px", marginLeft: "10px"}}>
+          Generate Paystub
+        </button>
+
+        <button className={`${Styles1.button}`} style={{padding: "20px", marginLeft: "10px"}}>
+          Retrieve Paystub
+        </button>
+
+        <button className={`${Styles1.button}`} style={{padding: "20px", marginLeft: "10px"}}>
+          Pay Employee
+        </button>
       </div>
 
     </React.Fragment>
--- frontend/renderer/pages/admin/adminHome.jsx
+++ frontend/renderer/pages/admin/adminHome.jsx
@@ -1,22 +1,66 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import Styles1 from '../../stylesheets/buttonComponent.module.scss';
+import Router  from 'next/router';
 
 function adminHome() {
+
+  const logOut = async () => {
+    try {
+        Router.push('/loginPage');    
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  const addEmployee = async () => {
+    try {
+        Router.push('/admin/addEmployee');    
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <React.Fragment>
       <Head>
         <title>Admin View</title>
       </Head>
 
+      <div style={{textAlign: "right"}}>
+        <button onClick={logOut} className={`${Styles1.button}`}>
+          Log out
+        </button>
+        </div>
+
       <div style={{textAlign: "center"}}>
         <img style={{padding:"0px 0px 0px 0px", height: "200px", width:"350px"}}src="/images/logo.png"/>
       </div>
 
       <div style={{textAlign: "center"}}>
-        <h1>This is the ADMIN Page!</h1>
+        <h1>Hello Admin!</h1>
+
+        <button onClick={(addEmployee)} className={`${Styles1.button}`} style={{padding: "20px", marginLeft: "10px"}}>
+          Add Employee
+        </button>
+
+        <button className={`${Styles1.button}`} style={{padding: "20px", marginLeft: "10px"}}>
+          Edit Employee
+        </button>
+
+        <button className={`${Styles1.button}`} style={{padding: "20px", marginLeft: "10px"}}>
+          Remove Employee
+        </button>
       </div>
 
+      <div style={{textAlign: "center"}}>
+        <button className={`${Styles1.button}`} style={{padding: "40px", marginTop: "10px"}}>
+          View Clock In / Clock Out Times
+        </button>
+      </div>
     </React.Fragment>
   );
 };
