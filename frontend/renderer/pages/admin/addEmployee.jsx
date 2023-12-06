22453774b1d8bb617a143c781fa800f7405e11f8
argjend12345 <argjend12345@gmail.com>
2023-12-05 23:22:34 -0500

    Worked on Admin pages & functions

--- frontend/renderer/pages/admin/addEmployee.jsx
+++ frontend/renderer/pages/admin/addEmployee.jsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Router  from 'next/router';
 import Styles from '../../stylesheets/homepage.module.scss';
 import Styles1 from '../../stylesheets/buttonComponent.module.scss';
+import ButtonStyle from '../../stylesheets/loadButton.module.scss';
 import Styles2 from '../../stylesheets/dropdown.module.scss';
 import axios from 'axios'
 
@@ -17,54 +18,79 @@ function addEmployee() {
   
   // Store employee data
   const [selectedRole, setSelectedRole] = useState(null);
-  const [username, setUsername]   = useState('');
-  const [password, setPassword]   = useState('');
-  const [name, setName]   = useState('');
-  const [hourlyPay, setHourlyPay]   = useState('');
+  const [username, setUsername]         = useState('');
+  const [password, setPassword]         = useState('');
+  const [name, setName]                 = useState('');
+  const [hourlyPay, setHourlyPay]       = useState('');
+  const [employeeId, setEmployeeId]     = useState('');
 
-  //Button states
+  //Used to keep track of selected inputs
+  const [monday, setMonday] = useState(false);
+  const [tuesday, setTuesday] = useState(false);
+  const [wednseday, setWednesday] = useState(false);
+  const [thursday, setThursday] = useState(false);
+  const [friday, setFriday] = useState(false);
+  const [saturday, setSaturday] = useState(false);
+  const [sunday, setSunday] = useState(false);
+
+  //Button loading
   const [isLoading, setIsLoading] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
   const [isError, setIsError]     = useState(false);
 
-  // Store days of the week
-  const [selectedDays, setSelectedDays] = useState({
-    Monday: false,
-    Tuesday: false,
-    Wednesday: false,
-    Thursday: false,
-    Friday: false,
-    Saturday: false,
-    Sunday: false,
-  });
-
-  // Checkbox function
-  const handleCheckboxChange = (day) => {
-    setSelectedDays((prevSelectedDays) => ({
-      ...prevSelectedDays,
-      [day]: !prevSelectedDays[day],
-    }));
-  };
+  const submitHandler = async () => {
+    try {
+        setIsLoading(true);
+        let isAuthenticated = false;
+        if(document.getElementById('content2').style.display === 'none')
+        {
+          isAuthenticated = await handleEmployeePost();
+        }else{
+          isAuthenticated = await updateClientShifts();
+        }
+         
+        if (isAuthenticated) {
+          setTimeout(() => {
+            setIsLoading(false);
+            setIsSuccess(true);
+            setIsError(false);
+            setTimeout(() => {
+              //If successful ~ loading content2 and hiding content1 
+              if(document.getElementById('content2').style.display === 'none')
+              {
+                setIsLoading(false);
+                setIsError(false);
+                setIsSuccess(false);
+                document.getElementById('content1').style.display='none';
+                document.getElementById('content2').style.display='inline';
+              }else{
+                //Push to next page.
+                Router.push('/admin/adminHome');
+              }
+           }, 1000);
+          }, 1000);
+        } else {
+          setTimeout(() => {
+            setIsLoading(false);
+            setIsSuccess(false);
+            setIsError(true);
+            setTimeout(() => {
+              setIsError(false);
+            }, 3000);
+  
+          }, 2000);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
   // Role functions
   const selectRole = (role) => {
     setSelectedRole(role);
     setIsOpen(false);
   };
-  const roles = [
-    'Employee',
-    'Admin',
-    'Accountant',
-  ];
-
-  // Hide / unhide styles
-  const [hidden, setHidden] = useState(false);
-  const hideStyle = {
-    display: hidden ? 'none' : ''
-  };
-  const showStyle = {
-    display: hidden ? '' : 'none'
-  }
+  const roles = ['Employee', 'Admin', 'Accountant',];
 
   // Dropdown functions
   const [isOpen, setIsOpen] = useState(false);
@@ -73,16 +99,68 @@ function addEmployee() {
     setIsOpen(!isOpen);
   };
 
-  // Submit button function
-  function createClient() 
-  {
-    //Need to check if inputs are valid/empty before posting...
-    if(handleEmployeePost(name, username, password, selectedRole, hourlyPay))
-    {
-      
-    }else{
-      //Display unsuccessful event ~ returned false. 
+  const handleEmployeePost = async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/employee',
+        {
+          name: name, 
+          username: username,
+          password: password,
+          role: selectedRole,
+          hourlyPay: hourlyPay
+        },
+        {
+          headers: {
+            Authorization: 'Bearer ' + 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTcwMTgzNTY1NCwiZXhwIjoxNzAxODcxNjU0fQ.hX-C7SrkJa6-srOxS09AzjB1IVMRxmgJENXaQjdnGHw'
+            //ipcRenderer.sendSync('getToken')
+          }
+        }
+      );
+      console.log(response.data);
+
+      setEmployeeId(response.data);//DB returns employee Id on successful post. We'll use later for updating employee shifts.
+  
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+      // Handle error...
     }
+  };
+
+  function updateClientShifts()
+  {
+      if (monday)
+      {
+        handleClientShifts(1, employeeId);
+      }
+      if(tuesday)
+      {
+        handleClientShifts(2, employeeId);
+      }
+      if(wednseday)
+      {
+        handleClientShifts(3, employeeId);
+      }
+      if(thursday)
+      {
+        handleClientShifts(4, employeeId);
+      }
+      if(friday)
+      {
+        handleClientShifts(5, employeeId);
+      }
+      if(saturday)
+      {
+        handleClientShifts(6, employeeId);
+      }
+      if(sunday)
+      {
+        handleClientShifts(7, employeeId);
+      }
+
+      return true;
   }
 
   return (
@@ -91,89 +169,140 @@ function addEmployee() {
         <title>Add Employee</title>
       </Head>
 
+      <div style={{textAlign: "Left"}}>
+        <button onClick={cancel} className={`${Styles1.button}`}>
+          Return
+        </button>
+      </div>
+
       <div style={{textAlign: "center"}}>
         <img style={{padding:"0px 0px 0px 0px", height: "200px", width:"350px"}}src="/images/logo.png"/>
-        <h2>Add Employee Information</h2>
-      
-        <div className={Styles2.dropdown}>
-          <button style={{width:"100px", height: "30px"}} className="dropdown-toggle" onClick={toggleDropdown}>
-            {selectedRole || 'Select a role'}
-          </button>
-          {isOpen && (
-            <ul className={Styles2.dropdownMenu} style={{position: "absolute"}}>
-              {roles.map((role, index) => (
-                <li key={index} onClick={() => selectRole(role)}>
-                  {role}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
 
         <div id="content1">
+          <h2>Add Employee Information</h2>
+        
+          <div className={Styles2.dropdown}>
+            <button style={{width:"100px", height: "30px"}} className="dropdown-toggle" onClick={toggleDropdown}>
+              {selectedRole || 'Select a role'}
+            </button>
+            
+            {isOpen && (
+              <ul className={Styles2.dropdownMenu} style={{position: "absolute"}}>
+                {roles.map((role, index) => (
+                  <li key={index} onClick={() => selectRole(role)}>
+                    {role}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+
           <div className={Styles.contact}>
-            <input type="text" id="name" placeholder='Name' onChange={(e) => setName(e.target.value)} style={hideStyle}/>
+            <input type="text" id="name" placeholder='Name' onChange={(e) => setName(e.target.value)}/>
           </div>
           <div className={Styles.contact}>
-            <input type="text" placeholder='Username' onChange={(e) => setUsername(e.target.value)} style={hideStyle}/>
+            <input type="text" placeholder='Username' onChange={(e) => setUsername(e.target.value)}/>
           </div>
           <div className={Styles.contact}>
-            <input type="text" placeholder='Password' onChange={(e) => setPassword(e.target.value)} style={hideStyle}/>
+            <input type="text" placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
           </div>
           <div className={Styles.contact}>
-            <input type="text" placeholder='Hourly Pay'onChange={(e) => setHourlyPay(e.target.value)} style={hideStyle}/>
+            <input type="text" placeholder='Hourly Pay'onChange={(e) => setHourlyPay(e.target.value)}/>
           </div>
           <div style={{textAlign: 'center'}}>   
-            <button id="submitB" onClick={createClient} className={`${Styles1.button} ${isLoading ? Styles1.loader : ''} ${isSuccess ? Styles1.success : ''} ${isError ? Styles1.error : ''}`} style={{height: "50px", width: "100px"}}>
-                {isSuccess ? 'Success' : isError ? 'Error' : 'Submit'}
-            </button> 
+            <button id="submitB" onClick={submitHandler} className={`${ButtonStyle.button} ${isLoading ? ButtonStyle.loader : ''} ${isSuccess ? ButtonStyle.success : ''} ${isError ? ButtonStyle.error : ''}`}>
+              {isSuccess ? 'Success' : isError ? 'Error' : 'Submit'}
+            </button>
           </div>
         </div>
 
-        <div id="content2" style={{textAlign: "center", display:'none'}}>
-          <h2>Select Employee's Shifts for the Week</h2>
-          {Object.keys(selectedDays).map((day) => 
-          (
-            <div key={day}>
-              <label>
-                <input type="checkbox" checked={selectedDays[day]} onChange={() => handleCheckboxChange(day)}/>
-                {day}
+        
+
+        <div id="content2" style={{display: 'none'}}>
+          <h2>Select Employee Shifts</h2>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <label style={{ marginRight: '10px' }}>
+                <input type="checkbox" onChange={() => setMonday(true)}/>
+                Monday
               </label>
             </div>
-          ))}
-          <div>
-            <p>Selected Days: {Object.keys(selectedDays).filter((day) => selectedDays[day]).join(', ')}</p>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <label style={{ marginRight: '10px' }}>
+                <input type="checkbox" onChange={() => setTuesday(true)}/>
+                Tuesday
+              </label>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <label style={{ marginRight: '10px' }}>
+                <input type="checkbox" onChange={() => setWednesday(true)}/>
+                Wednesday
+              </label>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <label style={{ marginRight: '10px' }}>
+                <input type="checkbox" onChange={() => setThursday(true)}/>
+                Thursday
+              </label>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <label style={{ marginRight: '10px' }}>
+                <input type="checkbox" onChange={() => setFriday(true)}/>
+                Friday
+              </label>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <label style={{ marginRight: '10px' }}>
+                <input type="checkbox" onChange={() => setSaturday(true)}/>
+                Saturday
+              </label>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <label style={{ marginRight: '10px' }}>
+                <input type="checkbox" onChange={() => setSunday(true)}/>
+                Sunday
+              </label>
+            </div>
+            <br></br>
+            <div style={{textAlign: 'center'}}>   
+              <button id="submitB" onClick={submitHandler} className={`${ButtonStyle.button} ${isLoading ? ButtonStyle.loader : ''} ${isSuccess ? ButtonStyle.success : ''} ${isError ? ButtonStyle.error : ''}`}>
+                {isSuccess ? 'Success' : isError ? 'Error' : 'Submit'}
+              </button>
+            </div>
           </div>
         </div>
 
       </div>
     
+
+      
+
+
       </React.Fragment>
   )
 }
 
-const handleEmployeePost = async (name, username, password, selectedRole, hourlyPay) => {
+const handleClientShifts = async (shiftId, employeeId) =>
+{
   try {
     const response = await axios.post(
-      'http://localhost:8080/employee',
+      'http://localhost:8080/employee/' +employeeId+ '/shifts/' +shiftId+ '/update',
       {
-        name: name, 
-        username: username,
-        password: password,
-        role: selectedRole,
-        hourlyPay: hourlyPay
+        id:"0",
+        shiftId: 1, 
+        clockIn: '00:00:00',
+        clockOut: '00:00:00',
+        scheduled: true,
       },
       {
         headers: {
-          Authorization: 'Bearer ' + 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTcwMTgwMjEzMCwiZXhwIjoxNzAxODM4MTMwfQ.o4FZzHSgpXi9WRnYEiKafoXbKNytf0jZMwdVfT4cM7Q'
+          Authorization: 'Bearer ' + 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTcwMTgzNTY1NCwiZXhwIjoxNzAxODcxNjU0fQ.hX-C7SrkJa6-srOxS09AzjB1IVMRxmgJENXaQjdnGHw'
           //ipcRenderer.sendSync('getToken')
         }
       }
     );
-    console.log(response.data);
-    //If successful ~ loading content2 and hiding content1 
-    document.getElementById("content1").style.display = "none";
-    document.getElementById("content2").style.display = "inline";
+
     return true;
   } catch (error) {
     console.log(error);
